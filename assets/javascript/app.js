3f642c45c4174f2a79318f3ad240700a5e2fb7a7
Pack Fan 1031 <ocskier@gmail.com>
2018-07-09 17:37:43 -0400

    first try

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,26 @@
 var intervalId;
+var questionlist = [
+    {question: "What is the largest rodent found in North America",
+     Answer: "Beaver",
+     Wrong: ["Rat", "Squirrel","Mouse"]}
+     ,{question: "The Dingo is a dog native to what country?", 
+     Answer: "Australia", 
+     Wrong: ["Spain", "United States", "Great Britain"]}];
 
-$(function() {
-
+$("#start").on("click", function() {
+    console.log(questionlist);
+    var i=0;
     function resetClock() {
+        $("#qna-box").empty();
         var number = 20;    
+        var newClock = $('<p id="clock" class="lead"></p>');
+        var newQuestion = $('<p id="question" class="lead">'+questionlist[i].question+'?</p>');
+        var newAnswerDiv = $('<div id="answers" class="list-group">');
+        for (var j=1; j<5; j++) {
+        newAnswerDiv.append('<button type="button" class="list-group-item list-group-item-action">'+questionlist[i].Answer+'</button>');
+        }
+        $("#qna-box").append(newClock).append(newQuestion).append(newAnswerDiv);
+        
         function run () {    
             clearInterval(intervalId);
             intervalId = setInterval(decrement, 1000);
@@ -24,9 +41,11 @@ $(function() {
       
               //  Alert the user that time is up.
               setTimeout(alert("Time Up!"), 1000 * 1);
+              i++;
+              resetClock();
             }
                 //  Show the number in the #show-number tag.
-                $("#clock").text("Time remaining: "+number + " secs!");
+            $("#clock").text("Time remaining: "+number + " secs!");
           }
       
           //  The stop function
@@ -37,14 +56,7 @@ $(function() {
             //  to the clearInterval function.
             clearInterval(intervalId);
           }
-      
-          //  Execute the run function.
-          run();
-
-    function gimmeAHanlder() {
-        return aHandler;
-      
+        run();
     }
-}
     resetClock();
 });
